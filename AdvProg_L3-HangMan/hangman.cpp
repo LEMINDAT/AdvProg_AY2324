221680d22132a0b9ea07e0e393301de4123dac33
le minh dat <23020037@vnu.edu.vn>
2024-03-27 02:35:23 +0700

    HangMan

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -67,8 +67,9 @@ bool isCharInWord(const char ch, const string& word)
 string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
-
-    return wordList[index];
+    string answer;
+    answer = wordList[index];
+    return answer;
 }
 
 /***
