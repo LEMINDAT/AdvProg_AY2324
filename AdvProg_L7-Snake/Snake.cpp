b1e972be2658bf87a09c777d8665709b093d4833
le minh dat <23020037@vnu.edu.vn>
2024-06-04 14:12:50 +0700

    snake & hello

--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -8,7 +8,7 @@ Snake::Snake(Game& _game, Position start)
     : head(new SnakeNode(start)), tail(head), game(_game), cherry(0)
 {
     // Using Game.snakeMoveTo method to move Snake to start position;
-    game.snakeMoveTo(start);    
+    game.snakeMoveTo(start);
 }
 
 // set some attributes as default value
@@ -16,15 +16,12 @@ Snake::Snake(Game& _game, Position start)
 
 Snake::~Snake()
 {
-    /*
-        Loop: SnakeNode p = tail; p != nullptr;
-        
-        Do:
-            SnakeNode* nextNode = p->next;
-            // delete p;
-            p = nextNode;
+   SnakeNode* p = tail;
+    while (p != nullptr) {
+        SnakeNode* nextNode = p->next;
+        delete p;
+        p = nextNode;
     }
-    */
 }
 
 // DO NOT CHANGE METHOD
@@ -36,30 +33,32 @@ vector<Position> Snake::getPositions() const
     return res;
 }
 
-/*** 
+/***
  * PLEASE UPDATE THIS METHOD
- * 
+ *
  * When snake moves to a position,
  *  head->next = ...
  *  head = head->next;
  * Args:
  * 		pos (newPosition): head of snake grow at new position
- * 
+ *
  * Returns:
  * 		// none
- * 
+ *
 ***/
 void Snake::growAtFront(Position newPosition)
 {
     // head of snake grow at new position
-	
-    /* YOUR CODE HERE */
+
+    SnakeNode* newNode = new SnakeNode(newPosition);
+    newNode->next = head;
+    head = newNode;
 }
 
 
-/*** 
+/***
  * PLEASE UPDATE THIS METHOD
- * 
+ *
  * When snake slide to a newPosition,
  *  if tail->next == nullptr: // snake has only one node
  *      // tail->position is assigned by new position.
@@ -73,88 +72,95 @@ void Snake::growAtFront(Position newPosition)
  *  	head->next = oldTailNode;
  * Args:
  * 		pos (newPosition): head of snake grow at new position
- * 
+ *
  * Returns:
  * 		// none
- * 
+ *
 ***/
 
 void Snake::slideTo(Position newPosition)
 {
-	if (tail->next == nullptr) { 
-        // position is assigned by new position.
-		/* YOUR CODE HERE */
-	}
-	else {
-		SnakeNode *oldTailNode = tail;
-		//cut the old tail off the snake
-        /* YOUR CODE HERE */
-		
-		// move it to the head of the snake
-        /* YOUR CODE HERE */
-		head = oldTailNode;
-	}
+	if (tail->next == nullptr) {
+        // If the snake has only one node, update its position
+        tail->position = newPosition;
+    }
+    else {
+        // Otherwise, remove the tail node and move it to the head
+        SnakeNode* oldTailNode = tail;
+        tail = tail->next;
+        oldTailNode->next = nullptr;
+        oldTailNode->position = newPosition;
+        head->next = oldTailNode;
+        head = oldTailNode;
+    }
 }
 
-/*** 
+/***
  * PLEASE UPDATE THIS METHOD
- * 
+ *
  * When snake eat a Cherry,
  *  // if snake eat cherry, number of cherries will increase one
  * Args:
  * 		// none
- * 
+ *
  * Returns:
  * 		// none
- * 
+ *
 ***/
 void Snake::eatCherry()
 {
-	/* YOUR CODE HERE */
+	 cherry++;
 }
 
-/*** 
+/***
  * PLEASE UPDATE THIS METHOD
- * 
+ *
  * When snake move in a direction,
  *  1. Create a newPosition and move:
  *      Position newPosition = head->position.move(direction);
  *  2. Using snakeMoveTo method to update game with newPosition
  *      game.snakeMoveTo(newPosition);
- *  3. 
- *      If gameOver, return ; 
+ *  3.
+ *      If gameOver, return ;
  *          return;
  *      elif cherry > 0:
  *          // If cherry > 0, cherry descrease one and growAtFront() with newPosition
  *          Note: Call to funtion growAtFront()
  *      else:
- *          // SlideTo() newPosition. 
+ *          // SlideTo() newPosition.
  *          Note: Call to function SlideTo()
  *
  * Args:
  * 		pos (newPosition): head of snake grow at new position
- * 
+ *
  * Returns:
  * 		// none
- * 
+ *
 ***/
 
 void Snake::move(Direction direction)
 {
+   // Create a new position based on the current head position and the direction
     Position newPosition = head->position.move(direction);
 
-    /* YOUR CODE HERE */
-    
-    // If gameOver, return ; 
-    /* YOUR CODE HERE */
+    // Move the snake to the new position
+    game.snakeMoveTo(newPosition);
 
-    // If cherry > 0, cherry descrease one and growAtFront() with newPosition
-    if (cherry > 0) {
-        /* YOUR CODE HERE */
-    } else {
-    	game.snakeLeave(tail->position);
-        /* YOUR CODE HERE */        
+    // Check if the game is over
+    if (game.isGameOver()) {
+        return;
     }
+
+    if (cherry > 0) {
+        // If there are cherries, decrease cherry count and grow the snake
+        cherry--;
+        growAtFront(newPosition);
+    }
+    else {
+        // Otherwise, slide the snake to the new position
+        slideTo(newPosition);
+    }
+
 }
 
 // DO NOT CHANGE METHOD
